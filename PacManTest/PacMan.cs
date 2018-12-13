d73058adb0fb35f6a2123a0df2498f16d8664f9e
Leandro Brás <shadowgaming.7@gmail.com>
2018-12-13 22:25:56 +0000

    created PacMan

---
+++ PacManTest/PacMan.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PacManTest {
+    class PacMan {
+        // Ascii ▄ ▀ █
+
+        private int x;
+        private int y;
+        private Dictionary<int, string[]> ghosts;
+        private int animation;
+        private int animationTimer;
+        private int animationSpeed;
+        public Direction direction;
+        private int moveSpeed;
+        private int speedTimer;
+
+        // Create a new read only string array that contains what we need to
+        //draw on the first animated frame of the ghost
+        private readonly string[] gFrame1 = new string[3] {
+            "▄██@▄",
+            "█████",
+            "▀███▀"
+        };
+        // Create a new read only string array that contains what we need to
+        //draw on the first animated frame of the ghost
+        private readonly string[] gFrame2 = new string[3] {
+            "▄█@█▀",
+            "███  ",
+            "▀███▄"
+        };
+
+
+        public PacMan(int x, int y, Direction direction) {
+            this.x = x;
+            this.y = y;
+            ghosts = new Dictionary<int, string[]> {
+                [0] = gFrame1,
+                [1] = gFrame2
+            };
+            animation = 0;
+            animationTimer = 0;
+            animationSpeed = 10;
+            this.direction = direction;
+
+            speedTimer = 0;
+            moveSpeed = 5;
+        }
+
+        public void Plot() {
+            animationTimer++;
+            if (animationTimer == animationSpeed) {
+                animation = animation == 0 ? 1 : 0;
+                animationTimer = 0;
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 3; i++) {
+                Console.SetCursorPosition(x, y + i);
+                Console.WriteLine(ghosts[animation][i]);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void UnPlot() {
+            for (int i = 0; i < 3; i++) {
+                Console.SetCursorPosition(x, y + i);
+                Console.WriteLine("     ");
+            }
+        }
+
+        public void Move() {
+            speedTimer++;
+
+            if (speedTimer == moveSpeed) {
+                speedTimer = 0;
+
+                switch (direction) {
+                    case Direction.Up:
+                        y--;
+                        break;
+                    case Direction.Down:
+                        y++;
+                        break;
+                    case Direction.Left:
+                        x--;
+                        break;
+                    case Direction.Right:
+                        x++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ PacManTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace PacManTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.CursorVisible = false;
+            Console.Clear();
+            PacMan pacMan = new PacMan(10, 10, Direction.Right);
+            ConsoleKeyInfo key = default(ConsoleKeyInfo);
+
+
+            while (true) {
+                pacMan.Plot();
+
+                Thread.Sleep(30);
+
+                pacMan.UnPlot();
+
+                switch (key.Key) {
+                    case ConsoleKey.A:
+                        pacMan.direction = Direction.Left;
+                        break;
+                    case ConsoleKey.D:
+                        pacMan.direction = Direction.Right;
+                        break;
+                    default:
+                        break;
+                }
+
+                pacMan.Move();
+            }
+        }
+    }
+}
